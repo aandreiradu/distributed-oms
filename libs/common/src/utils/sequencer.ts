b2374ae5d9665105173bf1b4004d1c7969ec7d0b
Andrei Radu <raduandrei697@gmail.com>
2024-11-19 01:04:48 +0200

    add sequencer

---
+++ libs/common/src/utils/sequencer.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import { LockResource } from './lock';
+
+interface SequencerProps {
+  code: string;
+  prefix?: string;
+  padding?: number;
+
+  rangeMin?: number;
+  rangeMax?: number;
+}
+
+@Injectable()
+export class Sequencer {
+  private sequenceCache: Record<string, number> = {};
+  constructor(private readonly prismaService: PrismaService) {}
+
+  private async sequencerFactory(code: string): Promise<number> {
+    switch (code) {
+      case 'ORDERS': {
+        const order = await this.prismaService.order.findFirst({
+          orderBy: { id: 'desc' },
+          select: { id: true },
+        });
+
+        return order?.id || 0;
+      }
+
+      default: {
+        throw new Error(`Unsupported code: ${code}`);
+      }
+    }
+  }
+
+  async getSequenceNumber(props: SequencerProps) {
+    console.log('this.sequenceCache is', this.sequenceCache);
+    return await LockResource.lock(
+      `${props.code}-SequencerLock`,
+      async () => {
+        if (!this.sequenceCache[props.code]) {
+          this.sequenceCache[props.code] = await this.sequencerFactory(
+            props.code,
+          );
+        }
+
+        const nextSequence = ++this.sequenceCache[props.code];
+        console.log('nextSequence', nextSequence);
+        let formattedNumber = nextSequence.toString();
+        console.log('formattedNumber', formattedNumber);
+
+        if (props.padding) {
+          formattedNumber = formattedNumber.padStart(props.padding, '0');
+          console.log('padding formattedNumber', formattedNumber);
+        }
+
+        if (props.prefix) {
+          formattedNumber = `${props.prefix}${formattedNumber}`;
+        }
+
+        return formattedNumber;
+      },
+      true,
+    );
+  }
+}
