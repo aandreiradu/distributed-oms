29f5f32008f7043104e48d63a4957ba34b7278e2
Andrei Radu <raduandrei697@gmail.com>
2024-12-09 23:57:20 +0200

    init redis module

---
+++ libs/common/src/redis/redis.module.ts
@@ -0,0 +1,26 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async (configSerice: ConfigService) => {
+        const redis = new Redis({
+          host: configSerice.getOrThrow<string>('REDIS_HOST'),
+          port: +configSerice.getOrThrow<string>('REDIS_PORT'),
+        });
+
+        redis.on('connect', () =>
+          console.info('Connected successfully to redis'),
+        );
+        redis.on('error', (error) =>
+          console.error('Failed to connect to redirs', error),
+        );
+      },
+    },
+  ],
+})
+export class RedisModule {}
